40fd7e8f51e7e207e20d9e3b1caeb9d8d91fd6ec
Tathagata Raha <tathagata.raha@research.iiit.ac.in>
2021-10-09 20:05:11 +0000

    Web3 configured partially

--- src/js/app.js
+++ src/js/app.js
@@ -24,20 +24,83 @@ App = {
   },
 
   initWeb3: async function() {
-    /*
-     * Replace me...
-     */
-
+    if (typeof web3 !== 'undefined') {
+      // If a web3 instance is already provided by Meta Mask.
+      App.web3Provider = web3.currentProvider;
+      web3 = new Web3(web3.currentProvider);
+    } else {
+      // Specify default instance if no web3 instance provided
+      App.web3Provider = new Web3.providers.HttpProvider('https://tathagata-raha-tanmaysachan-dice-game-dapp-xg5wx4xj3vjjj-8545.githubpreview.dev/');
+      web3 = new Web3(App.web3Provider);
+    }
     return App.initContract();
   },
 
   initContract: function() {
-    /*
-     * Replace me...
-     */
-
-    return App.bindEvents();
+    $.getJSON("Netflix.json", function(netflix) {
+      // Instantiate a new truffle contract from the artifact
+      App.contracts.Netflix = TruffleContract(netflix);
+      // Connect provider to interact with contract
+      App.contracts.Netflix.setProvider(App.web3Provider);
+      // App.listenForEvents();
+      return App.render();
+    });
   },
+  render: function() {
+    var electionInstance;
+    var content = $("#content");
+    
+    // Load account data
+    web3.eth.getCoinbase(function(err, account) {
+      if (err === null) {
+        App.account = account;
+        $("#accountAddress").html("Your Account: " + account);
+      }
+      else{
+        $("#accountAddress").html("Account not detected");
+        console.log("Account not detected")
+      }
+    });
+
+    // Load contract data
+  //   App.contracts.Election.deployed().then(function(instance) {
+  //     electionInstance = instance;
+  //     return electionInstance.candidatesCount();
+  //   }).then(function(candidatesCount) {
+  //     var candidatesResults = $("#candidatesResults");
+  //     candidatesResults.empty();
+
+  //     var candidatesSelect = $('#candidatesSelect');
+  //     candidatesSelect.empty();
+
+  //     for (var i = 1; i <= candidatesCount; i++) {
+  //       electionInstance.candidates(i).then(function(candidate) {
+  //         var id = candidate[0];
+  //         var name = candidate[1];
+  //         var voteCount = candidate[2];
+
+  //         // Render candidate Result
+  //         var candidateTemplate = "<tr><th>" + id + "</th><td>" + name + "</td><td>" + voteCount + "</td></tr>"
+  //         candidatesResults.append(candidateTemplate);
+
+  //         // Render candidate ballot option
+  //         var candidateOption = "<option value='" + id + "' >" + name + "</ option>"
+  //         candidatesSelect.append(candidateOption);
+  //       });
+  //     }
+  //     return electionInstance.voters(App.account);
+  //   }).then(function(hasVoted) {
+  //     // Do not allow a user to vote
+  //     if(hasVoted) {
+  //       $('form').hide();
+  //     }
+  //     loader.hide();
+  //     content.show();
+  //   }).catch(function(error) {
+  //     console.warn(error);
+  //   });
+  },
+
 
   bindEvents: function() {
     $(document).on('click', '.btn-adopt', App.handleAdopt);
